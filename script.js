79fab06017480b53f41ccfb09204b11f7cd87985
Ivan Azoyan <azoyan.89@mail.ru>
2021-06-05 01:00:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,52 +10,88 @@ function copy() {
     document.execCommand("copy");
 }
 
+function pad(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
+
+class Time {
+    constructor(time_str) {
+        let raw_strings = time_str.split(':').reverse()
+        this.seconds = parseInt(raw_strings[0] ? raw_strings[0] : "00")
+        this.minutes = parseInt(raw_strings[1] ? raw_strings[1] : "00")
+        this.hours = parseInt(raw_strings[2] ? raw_strings[2] : "00")
+    }
+
+    tostring() {
+        let s = '' + pad(this.hours, 2) + ':' + pad(this.minutes, 2) + ':' + pad(this.seconds, 2) + '.000'
+        return s
+    }
+    add_one_minute() {
+        if ((this.minutes + 1) > 60) {
+            this.hours += 1
+            this.minutes = 0
+        } else {
+            this.minutes += 1
+        }
+        return this
+    }
+}
+
+String.prototype.decodeHTML = function() {
+    var map = {"gt":">" /* , … */};
+    return this.replace(/&(#(?:x[0-9a-f]+|\d+)|[a-z]+);?/gi, function($0, $1) {
+        if ($1[0] === "#") {
+            return String.fromCharCode($1[1].toLowerCase() === "x" ? parseInt($1.substr(2), 16)  : parseInt($1.substr(1), 10));
+        } else {
+            return map.hasOwnProperty($1) ? map[$1] : $0;
+        }
+    });
+};
+
+function downloadFile() {
+    translate()
+    let text = document.getElementById("target").innerHTML
+    text = text.decodeHTML()
+    var filename = "WEBVTT.vtt";
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
 
 function translate() {
-    var str = document.getElementById("source").value;
-    let lines = str.split(/\r?\n/);
+    let str = document.getElementById("source").value;
 
     let result = 'WEBVTT\nKind: captions\nLanguage: en\n\n'
 
+    const regex = new RegExp(/(?:\d\d(?::)\d\d)/)
+    const regex_multiline = new RegExp(/(?:\d\d(?::)\d\d)/igm)
+
+    let descriptions = str.split(regex)
+    descriptions = descriptions.filter(function (el) {
+        return el != null && el.length > 0
+    })
+    for (i = 0; i < descriptions.length; ++i) {
+        descriptions[i] = descriptions[i].replace("➡️", '').trim()
+    }
+    descriptions.forEach(element => {
+        return element.replace("➡️", '')
+    });
+    console.log(descriptions)
     let timecodes = []
-    let descriptions = []
-    for (let i = 0; i < lines.length; ++i) {
-        let splitted_line = lines[i].split('➡️')
-        console.log(splitted_line)
-        if (splitted_line.length > 1) {
-            timecodes.push(splitted_line[0].replace(/(^[\s]+|[\s]+$)/g, ''))
-            descriptions.push(splitted_line[1].replace(/(^[\s]+|[\s]+$)/g, ''))
-        }
-    }
 
-    function pad(num, size) {
-        num = num.toString();
-        while (num.length < size) num = "0" + num;
-        return num;
+    while (match = regex_multiline.exec(str)) {
+        timecodes.push(match.toString())
     }
-
-    class Time {
-        constructor(time_str) {
-            let raw_strings = time_str.split(':').reverse()
-            this.seconds = parseInt(raw_strings[0] ? raw_strings[0] : "00")
-            this.minutes = parseInt(raw_strings[1] ? raw_strings[1] : "00")
-            this.hours = parseInt(raw_strings[2] ? raw_strings[2] : "00")
-        }
-
-        tostring() {
-            let s = '' + pad(this.hours, 2) + ':' + pad(this.minutes, 2) + ':' + pad(this.seconds, 2) + '.000'
-            return s
-        }
-        add_one_minute() {
-            if ((this.minutes + 1) > 60) {
-                this.hours += 1
-                this.minutes = 0
-            } else {
-                this.minutes += 1
-            }
-            return this
-        }
-    }
+    console.log(timecodes)
 
     let vtt = []
     for (let i = 0; i < timecodes.length; ++i) {
